0397568a4b0248c709ffd8e90b0ef0f975acb3dd
Andrey Chebotar <andreyoneup@gmail.com>
2019-12-14 13:39:30 +0500

    Refactoring

--- db_conn.php
+++ db_conn.php
@@ -11,7 +11,7 @@ function db_connection() {
   // Connect to MySQL
   $conn = new mysqli($servername, $username, $password);
   if ($conn->connect_error) {
-      die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
   }
 
   $db = array(
@@ -23,12 +23,12 @@ function db_connection() {
 
   // If database is not exist create one
   if (!mysqli_select_db($conn,$dbName)){
-      $sql = "CREATE DATABASE ".$dbName;
-      if ($conn->query($sql) === TRUE) {
-          echo "Database created successfully";
-      }else {
-          echo "Error creating database: " . $conn->error;
-      }
+    $sql = "CREATE DATABASE ".$dbName;
+    if ($conn->query($sql) === TRUE) {
+        echo "Database created successfully";
+    }else {
+        echo "Error creating database: " . $conn->error;
+    }
   }
   
   // If table does not exists create one
