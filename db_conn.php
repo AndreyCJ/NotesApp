da3dc12b641d85b0f394e2d696697c18b3663754
Andrey Chebotar <andreyoneup@gmail.com>
2019-12-14 13:28:56 +0500

    Refactoring

--- db_conn.php
+++ db_conn.php
@@ -2,15 +2,47 @@
 
 function db_connection() {
   global $db;
+
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbName = "todoList";
+
+  // Connect to MySQL
+  $conn = new mysqli($servername, $username, $password);
+  if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+  }
+
+  // If database is not exist create one
+  if (!mysqli_select_db($conn,$dbName)){
+      $sql = "CREATE DATABASE ".$dbName;
+      if ($conn->query($sql) === TRUE) {
+          echo "Database created successfully";
+      }else {
+          echo "Error creating database: " . $conn->error;
+      }
+  }
   
-  $conn = mysqli_connect('localhost', 'root', '', 'todolist') or die("Couldn't connect to database.");
-  
+  // If table does not exists create one
+  mysqli_set_charset($db["connect"], 'utf8');
+
+  $query = "SELECT ID FROM todo";
+  $result = mysqli_query($conn, $query);
+
+  if(empty($result)) {
+    $query = "CREATE TABLE todo (
+      id int(11) NOT NULL,
+      todoTitle varchar(200) NOT NULL,
+      todoDescription text NOT NULL,
+      done tinyint(1) DEFAULT 0
+    )";
+    $result = mysqli_query($conn, $query);
+  }
 
   $db = array(
     "connect" => $conn,
     "table" => "todo"
   );
-
-  mysqli_set_charset($db["connect"], 'utf8');
   return $db;
 }
--- insertData.php
+++ insertData.php
@@ -13,10 +13,11 @@ class insertData {
     global $db;
     $db["connect"];
     $table = $db["table"];
+    $title = $_POST['titleData'];
 
-    if ($_POST['titleData'] == "") {
+    if ($title == "") {
       $description = mysqli_real_escape_string($db["connect"], $_POST['descriptionData']);
-      $query = "INSERT INTO $table (todoDescription) values ('$description')";
+      $query = "INSERT INTO $table (todoTitle, todoDescription) values ('', '$description')";
       if (mysqli_query($db["connect"], $query)) {
         echo json_encode("Запись добавлена", JSON_UNESCAPED_UNICODE);
         mysqli_close($db["connect"]);
@@ -25,7 +26,6 @@ class insertData {
         mysqli_close($db["connect"]);
       }
     } else {
-      $title = $_POST['titleData'];
       $description = mysqli_real_escape_string($db["connect"], $_POST['descriptionData']);
       $query = "INSERT INTO $table (todoTitle, todoDescription) values ('$title', '$description')";
       if (mysqli_query($db["connect"], $query)) {
